9b5936cf98ceddd4255ff7e1c6c5ebe4111c3886
EC2 Default User <ec2-user@ip-172-31-39-231.us-east-2.compute.internal>
2021-02-10 05:18:07 +0000

    hw1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,62 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum arr.max(2)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  a = arr.combination(2).map{ |x| sum x }.index n
+  if a == nil then
+    return false
+  end
+  return true
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant? ss
+  ss =~ /^[bcdfghjklmnpqrstvwxyz]/i
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4? ss
+  ss =~ /^[01]+100$|^100$|^0$/
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :price, :isbn
+  def initialize(ii, p)
+    if  ii == ''
+      raise ArgumentError.new("ISBN must be non empty string")
+    end
+    if p <= 0 
+      raise ArgumentError.new("Price must be greater than zero")
+    end
+    @isbn = ii
+    @price = p
+  end
+  def price=(p)
+    if p <= 0
+      raise ArgumentError.new("Price must be greater than zero")
+    end
+    @price = p
+  end
+  def isbn=(ii)
+    if ii == ''
+      raise ArgumentError.new("ISBN must be non empty string")
+    end
+    @isbn = ii
+  end
+  def price_as_string
+    "$#{format "%.2f", @price}"
+  end
 end
